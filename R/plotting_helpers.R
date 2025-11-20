827108882ba3f8540c9c496f3427052a96047327
Nahian Ahmed <nahian.shafin.ahmed@gmail.com>
2025-11-20 01:29:56 -0800

    ...

--- R/plotting_helpers.R
+++ R/plotting_helpers.R
@@ -102,15 +102,16 @@ plot_sites <- function(
         # )
 
         theme(
+            
             # --- 1. Pull Title Closer ---
             # Negative bottom margin (b = -15) pulls the title down towards the map
-            plot.title = element_text(hjust = 0.5, face = "bold", margin = margin(b = -25)),
+            plot.title = element_text(hjust = 0.5, face = "bold", margin = margin(b = -50)),
 
             # --- 2. Legend Position & Margins ---
             legend.position = "bottom",
             legend.direction = "horizontal",
             # Negative top margin (t = -15) pulls the legend up towards the map
-            legend.margin = margin(t = -25),
+            legend.margin = margin(t = -50),
             # Remove extra box spacing
             legend.box.margin = margin(0, 0, 0, 0),
 
