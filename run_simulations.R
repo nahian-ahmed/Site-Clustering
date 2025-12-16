4324c639440ef4dfb1ff8e39cc6285b80aa5f894
Nahian Ahmed <nahian.shafin.ahmed@gmail.com>
2025-12-15 22:44:24 -0800

    ...

--- run_simulations.R
+++ run_simulations.R
@@ -56,10 +56,10 @@ n_fit_repeats <- 25
 n_test_repeats <- 25
 
 
-simulate_only <- TRUE # Debug override
-n_simulations <- 1 # Debug override
-n_fit_repeats <- 1 # Debug override
-n_test_repeats <- 1 # Debug override
+# simulate_only <- TRUE # Debug override
+# n_simulations <- 1 # Debug override
+# n_fit_repeats <- 1 # Debug override
+# n_test_repeats <- 1 # Debug override
 
 
 res_m <- 100 
