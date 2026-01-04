0b9a6aaf84a26332468c8929782a104036aaba8f
Nahian Ahmed <nahian.shafin.ahmed@gmail.com>
2026-01-04 05:45:57 -0800

    ...

--- simulations.R
+++ simulations.R
@@ -32,7 +32,7 @@ set.seed(123)
 
 # --- Simulation repetitions ---
 n_sims <- 100 
-n_sims <- 3 # FOR DEBUGGING
+# n_sims <- 3 # FOR DEBUGGING
 
 # --- Model fitting repetitions ---
 n_reps <- 30 
@@ -74,7 +74,7 @@ sampling_sd <- 5           # SD for Gaussian smoothing (in site units, ~25 cells
 # --- Spatial Autocorrelation (SAC) Settings ---
 sac_levels <- c("Low", "Medium", "High") 
 # sac_sigmas <- c(Low = 0, Medium = 5, High = 15)
-sac_sigmas <- c(Low = 0, Medium = 3, High = 9) 
+sac_sigmas <- c(Low = 0, Medium = 3, High = 6) 
 
 # --- Skew Patterns ---
 # Fixed to Centers
