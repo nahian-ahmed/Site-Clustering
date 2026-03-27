e110167e27f8d070dea16e8415d3ef0003a1dc16
Nahian Ahmed <nahian.shafin.ahmed@gmail.com>
2026-03-27 11:44:54 -0700

    ...

--- plot_species_clusters.R
+++ plot_species_clusters.R
@@ -820,7 +820,7 @@ for (sp in species_list) {
         labs(title = col_title, y = row_label) +
         theme(
           legend.position = "bottom", legend.text = element_text(size = 18), legend.title = element_text(size = 20, vjust = 1), 
-          legend.key.width = unit(1, "cm"), legend.box.margin = margin(t = 20), plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
+          legend.key.width = unit(3, "cm"), legend.box.margin = margin(t = 20), plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
           axis.title.y = element_text(angle = 90, size = 20, face = "bold", margin = margin(r = 10))
         )
       
@@ -836,7 +836,7 @@ for (sp in species_list) {
   
   # Save the scales plot directly, forcing a white background
   ggsave(file.path(map_output_dir, paste0(sp, "_scales.png")), 
-         plot = final_scales, width = 16, height = 30, dpi = 300, 
+         plot = final_scales, width = 14, height = 32, dpi = 240, 
          bg = "white") # <--- ADDED bg = "white"
   
 }
