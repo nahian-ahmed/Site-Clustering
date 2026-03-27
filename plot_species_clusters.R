f90ed1fb6667ec421e38b043fc0d1fb234c3686e
Nahian Ahmed <nahian.shafin.ahmed@gmail.com>
2026-03-27 12:40:41 -0700

    ...

--- plot_species_clusters.R
+++ plot_species_clusters.R
@@ -838,7 +838,7 @@ for (sp in species_list) {
   
   # Save the scales plot directly, forcing a white background
   ggsave(file.path(map_output_dir, paste0(sp, "_scales.png")), 
-         plot = final_scales, width = 14, height = 45, dpi = 200, 
+         plot = final_scales, width = 16, height = 40, dpi = 200, 
          bg = "white") # <--- ADDED bg = "white"
   
 }
